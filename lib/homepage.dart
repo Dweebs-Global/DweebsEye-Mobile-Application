3207f899518787589f023031aee7504bac32507d
Elena <kolomeets.elena7@gmail.com>
2021-02-11 20:46:06 +0100

    Basic app for speech recognition

---
+++ lib/homepage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'input_output/mic_speech.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage(this.title);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isListening = false;
+  String text = "Microphone input goes here.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 60.0),
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 150,
+        width: 150,
+        child: FloatingActionButton(
+          child: Icon(
+            isListening ? Icons.mic : Icons.mic_none,
+            size: 70.0,
+          ),
+          tooltip: "Get microphone input",
+          onPressed: toggleRecording,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Future toggleRecording() => MicSpeech.toggleRecording(
+        onListening: (isListening) {
+          setState(() => this.isListening = isListening);
+        },
+        onResult: (text) => setState(() => this.text = text),
+      );
+}
