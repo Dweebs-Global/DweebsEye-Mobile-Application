8de5e634868ef542696c7d5d67254f7a31aa9c8c
Shesha Shankar <sjan0009@student.monash.edu>
2021-03-01 12:59:48 +1100

    Completing image and video capture tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+  runApp(MyApp(firstCamera));
 }
 
 class MyApp extends StatelessWidget {
   static final String title = "DweebsEye";
+  CameraDescription firstCamera;
+  MyApp(CameraDescription firstCamera)
+  {
+    this.firstCamera = firstCamera;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: HomePage(title),
+      home: HomePage(title,firstCamera),
     );
   }
 }
