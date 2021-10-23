cbd3a9e43000012cb404c4746b76193d2119a09b
jason <5660582+SmoothMaverick@users.noreply.github.com>
2021-10-23 03:03:12 -0700

    Remove firebase dependency

--- lib/platform/mobile.dart
+++ lib/platform/mobile.dart
@@ -1,8 +1,10 @@
 import 'myplatform.dart';
 import 'dart:io';
+
 class PlatformUtil {
   static MyPlatform myPlatform() {
     if (Platform.isAndroid) return MyPlatform.ANDROID;
     if (Platform.isIOS) return MyPlatform.IOS;
+    return null;
   }
 }
